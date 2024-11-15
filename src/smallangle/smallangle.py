95e28b6b5da53d2d1997d80cda82674b56c1b71b
MissaFissa <meho11@outlook.com>
2024-11-15 14:46:00 +0100

    final commit

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,20 +1,50 @@
+"""Generate lists of numbers between 0 and 2π and their repesctive sine or tangent values.
+"""
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def smallangle():
+    """Make a group of commands using the @click.group() decorator.
+    """
+    pass
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@smallangle.command()
+@click.option(
+    "-n",
+    "--count",
+    default = 1,
+)
+def sin(count):
+    """Generate list of numbers between 0 and 2π with their respective sine values.
+
+    Args:
+        count (int): number of x-values between 0 and 2π.
+    """
+    x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
+
     print(df)
 
+@smallangle.command()
+@click.option(
+    "-n",
+    "--count",
+    default = 1,
+)
+def tan(count):
+    """Generate list of numbers between 0 and 2π with their respective tangent values.
 
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    Args:
+        count (int): number of x-values between 0 and 2π.
+    """
+    x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
-    print(df)
 
+    print(df)
 
 if __name__ == "__main__":
-    sin(10)
+
+    smallangle()
